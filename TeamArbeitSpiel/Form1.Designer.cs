e27daeba517865137ce64fcf9f6e8d269147cd7c
Lian Eifert <lian_eifert@sluz.ch>
2022-04-11 08:59:25 +0200

    Projektdateien hinzufügen.

---
+++ TeamArbeitSpiel/Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿
+namespace TeamArbeitSpiel
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txbAnzSpieler = new System.Windows.Forms.TextBox();
+            this.lblAnzSpieler = new System.Windows.Forms.Label();
+            this.btnPlay = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txbAnzSpieler
+            // 
+            this.txbAnzSpieler.Location = new System.Drawing.Point(304, 111);
+            this.txbAnzSpieler.Name = "txbAnzSpieler";
+            this.txbAnzSpieler.Size = new System.Drawing.Size(186, 38);
+            this.txbAnzSpieler.TabIndex = 0;
+            // 
+            // lblAnzSpieler
+            // 
+            this.lblAnzSpieler.AutoSize = true;
+            this.lblAnzSpieler.Location = new System.Drawing.Point(90, 111);
+            this.lblAnzSpieler.Name = "lblAnzSpieler";
+            this.lblAnzSpieler.Size = new System.Drawing.Size(208, 32);
+            this.lblAnzSpieler.TabIndex = 1;
+            this.lblAnzSpieler.Text = "Anzahl Spieler:";
+            // 
+            // btnPlay
+            // 
+            this.btnPlay.Location = new System.Drawing.Point(96, 215);
+            this.btnPlay.Name = "btnPlay";
+            this.btnPlay.Size = new System.Drawing.Size(578, 284);
+            this.btnPlay.TabIndex = 2;
+            this.btnPlay.Text = "Spielen";
+            this.btnPlay.UseVisualStyleBackColor = true;
+            this.btnPlay.Click += new System.EventHandler(this.btnPlay_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1119, 747);
+            this.Controls.Add(this.btnPlay);
+            this.Controls.Add(this.lblAnzSpieler);
+            this.Controls.Add(this.txbAnzSpieler);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txbAnzSpieler;
+        private System.Windows.Forms.Label lblAnzSpieler;
+        private System.Windows.Forms.Button btnPlay;
+    }
+}
+
---
+++ TeamArbeitSpiel/Form1.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TeamArbeitSpiel
+{
+    public partial class Form1 : Form
+    {
+        List<int> Spielerzahlen = new List<int>();
+        List<int> Zahlen = new List<int>();
+
+        int RandomZahl;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+
+
+        private void btnPlay_Click(object sender, EventArgs e)
+        {
+            Random Rand = new Random();
+             RandomZahl = Rand.Next(1, 100);
+
+            int AnzSpieler = Convert.ToInt32(txbAnzSpieler.Text);
+
+            btnPlay.Visible = false;
+            txbAnzSpieler.Visible = false;
+            lblAnzSpieler.Text = "Jeder Spieler gibt die Zahl von 1 bis 10 ein. Die Zahl ist: " + RandomZahl;
+
+            Button Zahlanschauen = new Button();
+            Zahlanschauen.Text = "Drücken um die Zahl des ersten Spielers zu sehen";
+            Zahlanschauen.Top = 100;
+            Zahlanschauen.Left = 100;
+            Zahlanschauen.Width = 280;
+            Zahlanschauen.Click += Zahlenanschauen_Click;
+            this.Controls.Add(Zahlanschauen);
+
+            int zähler = 1;
+            int LblTop = 200;
+
+            for(int i = 0;i < AnzSpieler;i++)
+            {
+                int RandomZahl2 = Rand.Next(1, 100);
+
+                Zahlen.Add(RandomZahl2);
+
+                Label Text = new Label();
+                Text.Text = "Spieler " + zähler;
+                Text.Left = 100;
+                Text.Top = LblTop;
+                this.Controls.Add(Text);
+                
+                TextBox Zahl = new TextBox();
+                Zahl.Left = 200;
+                Zahl.Top = LblTop;
+                this.Controls.Add(Zahl);
+                Zahl.TextChanged += Textgeändert;
+
+                LblTop = LblTop + 40;
+                zähler++;
+
+
+            }
+
+            
+
+        }
+        private void Textgeändert(object sender, EventArgs e)
+        {
+            
+            TextBox temp = (TextBox)sender;
+
+                Spielerzahlen.Add(Convert.ToInt32(temp.Text));
+            
+            if (Spielerzahlen.Count == Convert.ToInt32(txbAnzSpieler.Text))
+            {
+                BeginneAuswertung();
+            }
+
+        
+        }
+
+        private void Voting()
+        {
+
+            for(int i = 0; i < )
+
+
+        }
+        private void BeginneAuswertung()
+        {
+
+            Voting();
+
+
+
+           int indexdesgrössten = Spielerzahlen.IndexOf(Spielerzahlen.Max());
+
+            int unterschied = Math.Abs(Zahlen[indexdesgrössten] - RandomZahl);
+            for(int i = 0; i < Zahlen.Count;i++)
+            {
+                if (Math.Abs(Zahlen[i] - RandomZahl) < unterschied)
+                {
+                    MessageBox.Show("Verloren");
+                    Close();
+                }
+                else
+                    MessageBox.Show("Gewonnen");
+                Close();
+            }
+        }
+        private void Zahlenanschauen_Click(object sender, EventArgs e)
+        {
+                int Spieler = 1;
+
+            foreach (int zahl in Zahlen)
+            {
+                string t = zahl.ToString();
+
+                if (DialogResult.OK == MessageBox.Show("Spieler "+Spieler+" hat die Zahl: "+t,"OK drücken wenn die Zahl gemerkt wurde",MessageBoxButtons.OK,MessageBoxIcon.Information))
+                {
+
+                }
+                if (DialogResult.OK == MessageBox.Show("Der nächste Spieler ist dran","Drücken für nächste Zahl", MessageBoxButtons.OK, MessageBoxIcon.Information))
+                {
+
+                }
+                Spieler++;
+
+            }
+
+
+        }
+
+    }
+}
