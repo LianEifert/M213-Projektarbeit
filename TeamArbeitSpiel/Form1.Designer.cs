31fb1a22473300eafe300b39b248f9297ca04fd8
Hao-Wang <89629020+C-x-3-3-0@users.noreply.github.com>
2022-05-26 10:56:19 +0200

    Upload the needed pictures and rework on the main page

--- TeamArbeitSpiel/Form1.Designer.cs
+++ TeamArbeitSpiel/Form1.Designer.cs
@@ -36,40 +36,46 @@ namespace TeamArbeitSpiel
             // 
             // txbAnzSpieler
             // 
-            this.txbAnzSpieler.Location = new System.Drawing.Point(304, 111);
+            this.txbAnzSpieler.Location = new System.Drawing.Point(350, 313);
+            this.txbAnzSpieler.Margin = new System.Windows.Forms.Padding(1, 1, 1, 1);
             this.txbAnzSpieler.Name = "txbAnzSpieler";
-            this.txbAnzSpieler.Size = new System.Drawing.Size(186, 38);
+            this.txbAnzSpieler.Size = new System.Drawing.Size(72, 20);
             this.txbAnzSpieler.TabIndex = 0;
+            this.txbAnzSpieler.Text = "3-9";
             // 
             // lblAnzSpieler
             // 
             this.lblAnzSpieler.AutoSize = true;
-            this.lblAnzSpieler.Location = new System.Drawing.Point(90, 111);
+            this.lblAnzSpieler.Font = new System.Drawing.Font("Nirmala UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAnzSpieler.Location = new System.Drawing.Point(250, 276);
+            this.lblAnzSpieler.Margin = new System.Windows.Forms.Padding(1, 0, 1, 0);
             this.lblAnzSpieler.Name = "lblAnzSpieler";
-            this.lblAnzSpieler.Size = new System.Drawing.Size(207, 32);
+            this.lblAnzSpieler.Size = new System.Drawing.Size(277, 25);
             this.lblAnzSpieler.TabIndex = 1;
-            this.lblAnzSpieler.Text = "Anzahl Spieler:";
+            this.lblAnzSpieler.Text = "Geben sie den Spieleranzahl an";
             // 
             // btnPlay
             // 
-            this.btnPlay.Location = new System.Drawing.Point(96, 215);
+            this.btnPlay.Location = new System.Drawing.Point(141, 173);
+            this.btnPlay.Margin = new System.Windows.Forms.Padding(1, 1, 1, 1);
             this.btnPlay.Name = "btnPlay";
-            this.btnPlay.Size = new System.Drawing.Size(578, 284);
+            this.btnPlay.Size = new System.Drawing.Size(478, 89);
             this.btnPlay.TabIndex = 2;
-            this.btnPlay.Text = "Spielen";
+            this.btnPlay.Text = "Spiel starten";
             this.btnPlay.UseVisualStyleBackColor = true;
             this.btnPlay.Click += new System.EventHandler(this.btnPlay_Click);
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1119, 747);
+            this.ClientSize = new System.Drawing.Size(813, 506);
             this.Controls.Add(this.btnPlay);
             this.Controls.Add(this.lblAnzSpieler);
             this.Controls.Add(this.txbAnzSpieler);
+            this.Margin = new System.Windows.Forms.Padding(1, 1, 1, 1);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "The close one out";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
